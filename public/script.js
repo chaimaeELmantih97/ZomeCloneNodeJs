378b78af30afd5607ee71c1cfd65ae437ce96e0a
chaimaeElmantih <elmantih.chaimae1997@gmail.com>
2021-06-25 12:11:47 +0100

    final changes

--- public/script.js
+++ public/script.js
@@ -1,15 +1,41 @@
 const socket = io("/");
-
 const videoGrid = document.getElementById("video-grid");
-const myVideo = document.createElement("video");
-myVideo.muted = true;
-
-var peer = new Peer(undefined, {
+const myPeer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
   port: "3030",
+  key: "oz9b3ni30qtcsor",
+  debug: 3,
+  config: {
+    iceServers: [
+      { url: "stun:stun.l.google.com:19302" },
+      {
+        url: "turn:numb.viagenie.ca:3478",
+        credential: "muazkh",
+        username: "web...@live.com",
+      },
+      {
+        url: "turn:numb.viagenie.ca",
+        credential: "muazkh",
+        username: "web...@live.com",
+      },
+      {
+        url: "turn:192.158.29.39:3478?transport=udp",
+        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+        username: "28224511:1379330808",
+      },
+      {
+        url: "turn:192.158.29.39:3478?transport=tcp",
+        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+        username: "28224511:1379330808",
+      },
+    ],
+  },
 });
 let myVideoStream;
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+const peers = {};
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -18,20 +44,52 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
-    peer.on("call", (call) => {
+    myPeer.on("call", (call) => {
       call.answer(stream);
       const video = document.createElement("video");
       call.on("stream", (userVideoStream) => {
         addVideoStream(video, userVideoStream);
       });
     });
+
     socket.on("user-connected", (userId) => {
       connectToNewUser(userId, stream);
     });
+    // input value
+    let text = $("input");
+    // when press enter send message
+    $("html").keydown(function (e) {
+      if (e.which == 13 && text.val().length !== 0) {
+        socket.emit("message", text.val());
+        text.val("");
+      }
+    });
+    socket.on("createMessage", (message) => {
+      $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+      scrollToBottom();
+    });
   });
-peer.on("open", (id) => {
-  socket.emit("join-room", roomId, id);
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
 });
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
 
 function addVideoStream(video, stream) {
   video.srcObject = stream;
@@ -41,10 +99,62 @@ function addVideoStream(video, stream) {
   videoGrid.append(video);
 }
 
-function connectToNewUser(userId, stream) {
-  const call = peer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
-  });
-}
+const scrollToBottom = () => {
+  var d = $(".main__chat_window");
+  d.scrollTop(d.prop("scrollHeight"));
+};
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const playStop = () => {
+  console.log("object");
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
--- server.js
+++ server.js
@@ -6,23 +6,34 @@ const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
-const { v4: uuidv4 } = require("uuid");
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+const { v4: uuidV4 } = require("uuid");
+
 app.use("/peerjs", peerServer);
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+  res.redirect(`/${uuidV4()}`);
 });
+
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
+
 io.on("connection", (socket) => {
-  // join the room
   socket.on("join-room", (roomId, userId) => {
-    console.log("we have joined the room ");
     socket.join(roomId);
     socket.to(roomId).emit("user-connected", userId);
+    // messages
+    socket.on("message", (message) => {
+      //send message to the same room
+      io.to(roomId).emit("createMessage", message);
+    });
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
   });
 });
 
